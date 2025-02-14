9aa45ab9ce3cf61f4a0f2cd1f13eb89b3c5bdf07
aditprq <143347564+aditprq@users.noreply.github.com>
2025-02-14 11:45:05 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// 1. Install dependencies DONE
+// 2. Import dependencies DONE
+// 3. Setup webcam and canvas DONE
+// 4. Define references to those DONE
+// 5. Load posenet DONE
+// 6. Detect function DONE
+// 7. Drawing utilities from tensorflow DONE
+// 8. Draw functions DONE
+
+import React, { useRef } from "react";
+import "./App.css";
+
+import Canvas from "./components/Canvas";
+import MediaPose from "./components/MediaPose";
+
+function App() {
+
+  return (
+    // <Canvas/>
+    <MediaPose/>
+  );
+}
+
+export default App;
